c9f2e16e442989a906e09eec55e843743d14e1e9
Alejandro Garcia <50833238+Sertyron@users.noreply.github.com>
2019-08-11 16:09:58 -0300

    js Base de datos actualizada

--- js/master.js
+++ js/master.js
@@ -42,18 +42,27 @@ const horaLimite= (deadline , elem , finalMessage) =>{
     // json informacion de Equipos
   datos=[
     { titulo:"EMK",
-      texto:"eMonkeyz nació en noviembre de 2015 de la fusión de Celerius e-Sports , club con una larga historia en la escena nacional, y Bananized , grupo de profesionales con amplia experiencia en el sector de los deportes.",
-      jugadores:"../WebRivals/src/Jugadores/Monkey/220px-EMK_DuaLL_2019_Split_2.png"
+      texto:" eMonkeyz nació en noviembre de 2015 de la fusión de Celerius e-Sports , club con una larga historia en la escena nacional, y Bananized , grupo de profesionales con amplia experiencia en el sector de los deportes.",
+      jugadores:"../webRivals/src/Jugadores/Monkey/monkeyTeam.png",
     },
-    /*{ titulo:"EMK",
-      texto:"eMonkeyz nació en noviembre de 2015 de la fusión de Celerius e-Sports , club con una larga historia en la escena nacional, y Bananized , grupo de profesionales con amplia experiencia en el sector de los deportes.",
-      jugadores:"../src/Jugadores/220px-EMK_DuaLL_2019_Split_2.png"
+    { titulo:"Penguins",
+      texto:"Penguins es un club profesional de deportes electrónicos fundado en enero de 2016. Anteriormente se conocían como The G-Lab Penguins y The Penguins Mafia.",
+      jugadores:"../webRivals/src/Jugadores/Penguien/teamPenguin.png"
     },
-    { titulo:"EMK",
-      texto:"eMonkeyz nació en noviembre de 2015 de la fusión de Celerius e-Sports , club con una larga historia en la escena nacional, y Bananized , grupo de profesionales con amplia experiencia en el sector de los deportes.",
-      jugadores:"../src/Jugadores/220px-EMK_DuaLL_2019_Split_2.png"
-    },*/
-  ];
+    //va atener mas datos
+    { titulo:"S2V",
+      texto:"S2V Esports pertenece a S2V Digital Sports, una empresa española que se unió al negocio de esports en 2016.",
+      jugadores:"../webRivals/src/Jugadores/S2V/teamS2V.png"
+    },
+    { titulo:"Splyce Vipers",
+      texto:"Splyce es una organización de esports estadounidense con un equipo de la Liga Europea de Leyendas. La organización fue adquirida por OverActive Media en noviembre de 2018. Anteriormente se conocían como Follow eSports",
+      jugadores:"../webRivals/src/Jugadores/Slyce/teamStyle.png"
+    },
+    { titulo:"Team Queso",
+      texto:"Team Queso es un equipo español que compite en varios juegos. Actualmente están compitiendo como Telepizza Team Queso después del primer aniversario con su patrocinador Telepizza.",
+      jugadores:"../webRivals/src/Jugadores/TeamQueso/teamTeamQueso.png"
+    },
+  ]//no cerrar en json
 
     var marco=document.getElementById("marco");
 
@@ -65,17 +74,22 @@ const horaLimite= (deadline , elem , finalMessage) =>{
           let parrafo_p=document.createElement("p");
           parrafo_p.setAttribute("id","texto");
 
+        // preparacion de columna
+          let div_aling=document.createElement("div");
+          div_aling.setAttribute("class","columna");
+
           let jugadores_img=document.createElement("img");
           jugadores_img.setAttribute("id","jugador");
-          marco.appendChild(titulo_h2);
-          marco.appendChild(parrafo_p);
+          div_aling.appendChild(titulo_h2);
+          div_aling.appendChild(parrafo_p);
+          marco.appendChild(div_aling);
           marco.appendChild(jugadores_img);
 
         };
 
         crearEtiquetas();
 
-        function buscarEquipos(n=0){
+        function buscarEquipos(n){
           let elementos=datos[n];
           let tituloDOM = document.getElementById("titulo");
           tituloDOM.innerHTML = elementos.titulo;
@@ -86,19 +100,17 @@ const horaLimite= (deadline , elem , finalMessage) =>{
 
         };
 
-        buscarEquipos();
 
         //carga la info del equipo segun el numero del array
 
-        /*function cargaInfo(){
+        function cargaInfo(){
             let numeroDeEquipo=this.getAttribute("data-numero");
-
+            //el numero que toma la data es el numero de equipo
+            buscarEquipos(numeroDeEquipo);
         };
-          var claseSumar= document.getElementByClassName("link-equipo");
+          var claseSumar= document.getElementsByClassName("link-equipo");
 
         for(var i=0;i < claseSumar.length; i++){
           claseSumar[i].addEventListener('click', cargaInfo , false);
 
         };
-
-        cargaInfo();*/
